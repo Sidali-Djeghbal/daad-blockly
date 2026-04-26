3365322da441f0118288aa9879cd90db863ffd97
sidali-djeghbal <sidalidjeghbal@gmail.com>
2026-04-26 23:29:29 +0100

    Add pdo_mysql extension

--- test.php
+++ test.php
@@ -1,2 +1,4 @@
 <?php
-phpinfo();
+echo "Extensions: " . implode(", ", get_loaded_extensions()) . "\n";
+echo "PDO drivers: " . implode(", ", PDO::getAvailableDrivers()) . "\n";
+echo "MYSQL_HOST: " . getenv('MYSQL_HOST') . "\n";
